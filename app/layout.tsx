e3a1e04720511bd45b566fa48dd3e40e00fd7d3d
musharaf8651 <musharafz2k3@gmail.com>
2023-07-11 23:11:31 +0530

    completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 'use client'
+import Footer from '@/components/Footer'
 import '../styles/globals.css'
 import Navbar from '@/components/Navbar'
 import { ThemeProvider } from 'next-themes'
-
+import Head from './head'
 
 
 export default function RootLayout({
@@ -12,11 +13,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-     
+     <Head/>
       <body>
         <ThemeProvider enableSystem ={true} attribute="class">
         <Navbar />
         {children}
+        <Footer />
         </ThemeProvider>
         
         </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 'use-client' 
 import AboutSection from "@/components/AboutSection"
+import Footer from "@/components/Footer"
 import HeroSection from "@/components/HeroSection"
+import ProjectSection from "@/components/ProjectSection"
 export default function Home() {
   return (
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
       <HeroSection />
       <AboutSection />
+      <ProjectSection />
+      <Footer />
     </main>
   )
 }
